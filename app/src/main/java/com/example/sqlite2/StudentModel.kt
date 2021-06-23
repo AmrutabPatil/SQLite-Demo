786337c277a21a04bfcc83cc507200765f57cf21
AmrutabPatil <patilamruta4444gmail.com>
2021-06-23 16:35:34 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/sqlite2/StudentModel.kt
@@ -0,0 +1,21 @@
+package com.example.sqlite2
+
+import kotlin.random.Random
+
+data class StudentModel (
+    var id :Int=getAutoId(),
+    var name:String="",
+    var email:String=""
+
+
+){
+    companion object{
+        fun getAutoId():Int {
+            val random =Random
+            return random.nextInt(100)
+        }
+    }
+
+
+
+}
